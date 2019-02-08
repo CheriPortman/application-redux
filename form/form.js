017fad3a8d4ac77c7f77ddb1ed4cad7e8415df76
Cheri Portman <cheriportman@hotmail.com>
2019-02-08 12:05:23 -0800

    can console.log data from form

--- form/form.js
+++ form/form.js
@@ -1,6 +1,9 @@
-//see that the input is working
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const wishName = document.getElementById('wish-name');
 
 document.addEventListener('submit', function(event) {
     event.preventDefault();
     console.log('clicked');
+    console.log(wishName.value);
 });
