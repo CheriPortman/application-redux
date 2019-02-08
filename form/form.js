bbd379a33f6b2b12ee67bfe32a4babbea625b210
Cheri Portman <cheriportman@hotmail.com>
2019-02-08 15:27:48 -0800

    review page working, added styling and renamed some files

--- form/form.js
+++ form/form.js
@@ -3,14 +3,16 @@ const wishName = document.getElementById('wish-name');
 
 document.addEventListener('submit', function(event) {
     event.preventDefault();
-
+    
     const submission = {
         name: name.value,
         wishName: wishName.value
     };
 
+    console.log('submission', submission);
+
     const strungSubmission = JSON.stringify(submission);
     window.localStorage.setItem('submission', strungSubmission);
     //at this point I have a single submission in local storage
-    
+
 });
---
+++ review/review.js
@@ -0,0 +1,18 @@
+//grab my nodes
+const name = document.getElementById('name');
+const wishName = document.getElementById('wish-name');
+
+const jsonSubmission = window.localStorage.getItem('submission');
+const submission = JSON.parse(jsonSubmission);
+
+
+
+name.textContent = submission.name;
+wishName.textContent = submission.wishName;
+
+//at this point I have my data on the results page!
+
+//what do I want to do...
+//i have my data showing on the review page
+//i want to do what?
+//make two link buttons: accept input, redo inut
