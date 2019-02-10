64188f14366777c7ebf7410006c7240cf3013c4d
Cheri Portman <cheriportman@hotmail.com>
2019-02-09 19:12:21 -0800

    submission push to submissions successfully sent to and retrieved from localStorage

--- form/form.js
+++ form/form.js
@@ -4,41 +4,20 @@ const wishName = document.getElementById('wish-name');
 let submissions = [] ;
 
 document.addEventListener('submit', function(event) {
-    event.preventDefault();
+    // event.preventDefault();
     
     const submission = {
         name: name.value,
         wishName: wishName.value
     };
-    console.log('submission', submission);
     
     const jsonSubmissions = window.localStorage.getItem('submissions');
 
-//step by step get local storage parsed and console.logged appropriately
-
-
-
-
+    if(jsonSubmissions) {
+        submissions = JSON.parse(jsonSubmissions);
+    }
     submissions.push(submission);
-    console.log('submissions', submissions);
 
-
-    // console.log('jsonSubmissions', jsonSubmissions);
-    // let submissions = [];
-
-    // if(jsonSubmissions) {
-    //     submissions = JSON.parse(jsonSubmissions);
-    // }
-
-    
-    // const strungSubmissions = JSON.stringify(submissions);
-    // window.localStorage.setItem('submissions', strungSubmissions);
-    
-    // const strungSubmissions = JSON.stringify(submissions);
-
-    // console.log('strungSubmissions', strungSubmissions);
-
-    // window.localStorage.setItem('submissions', strungSubmissions);
-
-    // console.log('submissions', submissions);
+    const strungSubmissions = JSON.stringify(submissions);
+    window.localStorage.setItem('submissions', strungSubmissions);
 });
