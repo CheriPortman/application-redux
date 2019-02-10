1f4b4a7c3df4f2da316cc6a37fdc3971295ad411
Cheri Portman <cheriportman@hotmail.com>
2019-02-09 16:57:50 -0800

    plan made for making the list view of submissions

--- form/form.js
+++ form/form.js
@@ -15,4 +15,20 @@ document.addEventListener('submit', function(event) {
     window.localStorage.setItem('submission', strungSubmission);
     //at this point I have a single submission in local storage
 
+    let submissions = null;
+
+
+
+
+
+//current task: get submission pushed into an object submissions 
+    //happens before any stringifying.
+        //first, need to check to see if there is an object submissions. 
+            //if so, grab that object and parse it.
+            //then, current submission is pushed to the submissions array
+    //at this point, I should be able to 
+        //console.log('submissions', submissions);
+        //and get the JS side of data.
+    //stringify and setItem.
+
 });
---
+++ list/list.js
@@ -0,0 +1,14 @@
+//on this page, I want to get a list of each person in localStorage
+    //task before this page: make an object of submissions, done in the eventLister.    
+
+//Once I have an object, I will:
+    //create place in html for data to be rendered
+    //get Nodes into JS
+    //grab data
+    //parse the data
+    //get the data onto the list
+
+//Once I get a list, I need to make the name value into an anchor 
+    //send user to the user detail
+
+//then it's done!
