3a806973dd4461625da3ab56822beffe1c6148db
Cheri Portman <cheriportman@hotmail.com>
2019-02-11 13:04:53 -0800

    trying to fix issue with populating detail after form redirect"

--- review/review.js
+++ review/review.js
@@ -19,5 +19,12 @@ for(let i = 0; i < submissions.length; i++) {
         break;
     }
 }
+console.log('submission', submission);
 name.textContent = submission.name;
 wishName.textContent = submission.wishName;
+
+//the problem: I want to have the user land here after doing the form
+//right now, the submission is figured out using the searchParam so that
+//the link from the list is what drives populated table
+
+//However, I need the most recently done form to populate from the review page redirection
